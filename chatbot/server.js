c2f737833c5e04eb866c2fa28d739a3c56410186
MJ1717 <bananahamo77@gmail.com>
2025-05-30 16:38:33 -0400

    updated server js. app.post /chat

--- chatbot/server.js
+++ chatbot/server.js
@@ -5,12 +5,30 @@ const app = express();
 
 const PORT = 3000;
 
+app.use(express.json());
+
+//we do this becuz index.html is inside public folder
+
+
+
 app.get('/', (req, res) => {
     res.send('server is working');
 });
 
+app.post('/chat',(req, res) => {
+    console.log('Received POST /chat with body:', req.body);
+    const user_message = req.body.input_message;
+    console.log('User message:', user_message);
+    const bot_reply = `You said ${user_message}!`;
+    
+    res.json({'reply': bot_reply});
+})
+
+
 //run server
-app.listen(PORT);
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`);
+  });
 
 
 
